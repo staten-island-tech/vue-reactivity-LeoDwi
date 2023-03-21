aa232f1db2f69a2d7f63c32681acbf39d52f53f2
LeoDwi <leod13579@gmail.com>
2023-03-21 10:14:24 -0400

    Update array.js

--- vue-reactivity/src/array.js
+++ vue-reactivity/src/array.js
@@ -1,1 +1,55 @@
-const items = [e];
+const items = [];
+
+const ApiLinks = {
+  Breeds: "https://dog.ceo/api/breeds/list/all",
+  Images: `https://dog.ceo/api/breed/${DOMSelectors.input.value}/images/random`,
+};
+
+async function getImage(URL) {
+  try {
+    const response = await fetch(
+      `https://dog.ceo/api/breed/${URL}/images/random`
+    );
+    const image = await response.json();
+    console.log(`status code: ${response.status}`);
+    return image.message;
+  } catch (error) {
+    console.log(error);
+    clear();
+    DOMSelectors.parent.innerHTML = `<p class="error">${error}</p>`;
+  }
+}
+
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    const object = await response.json();
+    const Data = object.message;
+    const primaryBreeds = Object.keys(Data);
+    console.log(primaryBreeds);
+    const subBreeds = Object.values(Data).filter(
+      (object) => object.length >= 1
+    );
+    console.log(subBreeds);
+    const data = primaryBreeds.concat(...subBreeds);
+    console.log(data);
+    return primaryBreeds;
+  } catch (error) {
+    console.log(error);
+    clear();
+    DOMSelectors.parent.innerHTML = `<p class="error">${error}</p>`;
+  }
+}
+
+async function parentInserter(breed) {
+  let image = await getImage(breed);
+}
+
+function startUp() {
+  clear();
+  getData(ApiLinks.Breeds).then((data) => {
+    data.forEach((breed) => {
+      parentInserter(breed);
+    });
+  });
+}
