4d30df300727fa82c2a4bfe8c572c4b1710775ed
LeoDwi <leod13579@gmail.com>
2023-03-23 12:09:36 -0400

    i did it?

--- vue-reactivity/src/components/icons/cardTemplates.vue
+++ vue-reactivity/src/components/icons/cardTemplates.vue
@@ -1,5 +1,14 @@
+<script>
+import { dogs } from "../../array";
+export default {
+  data() {
+    return { dogs };
+  },
+};
+</script>
+
 <template>
   <div class="Card">
-    <h2 class="name"></h2>
+    <h2 :class="name" v-for="dog in dogs" v-bind:key="dog">{{ dog.breed }}</h2>
   </div>
 </template>
